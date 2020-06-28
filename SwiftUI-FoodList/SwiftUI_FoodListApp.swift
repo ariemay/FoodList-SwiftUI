2ef91cc4baf151e268440ab962360c6ec829103f
Arie May Wibowo <ariemayw@gmail.com>
2020-06-28 08:04:08 +0700

    Initial Commit

---
+++ SwiftUI-FoodList/SwiftUI_FoodListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FoodListApp.swift
+//  SwiftUI-FoodList
+//
+//  Created by Arie May Wibowo on 28/06/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FoodListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
